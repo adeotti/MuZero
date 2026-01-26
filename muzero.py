1a925804fde1819cf80b17b4b0d722623c7e1bad
Wafick <wafickachiambi@gmail.com>
2026-01-26 18:26:37 -0500

    Add code skeleton

---
+++ muzero.py
@@ -0,0 +1,68 @@
+import torch,sys,os,gymnasium_sudoku
+import torch.nn as nn
+import gymnasium as gym
+from gymnasium.vector import AsyncVectorEnv
+from dataclasses import dataclass
+from torch.utils.tensorboard import SummaryWritter
+
+
+class representation_net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+class dyanmic_net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def forward(self,x):
+        pass
+
+class prediction_net(nn.Module):
+    def __init__(self):
+        super().__innit__(self)
+        pass
+
+    def forward(self,x):
+        pass
+
+
+class mcts:
+    def __innit__(self):
+        pass
+
+
+class replay_buffer:
+    def __init__(self):
+        pass
+
+    def store(self):
+        pass
+
+    def sample(self):
+        pass
+
+
+class main:
+    def __innit__(self):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
+    
+    def log(self):
+        pass
+
+    def train(self):
+        pass
+     
+
+
+
