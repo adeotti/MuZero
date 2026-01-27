064c88211b7a8cafa6f3047e2d11dde07f337781
Wafick <wafickachiambi@gmail.com>
2026-01-27 14:14:19 -0500

    Update mcts skeleton

--- muzero.py
+++ muzero.py
@@ -1,38 +1,53 @@
-import torch,sys,os,gymnasium_sudoku
+import torch,sys,os,gymnasium_sudoku,mlflow
 import torch.nn as nn
 import gymnasium as gym
 from gymnasium.vector import AsyncVectorEnv
 from dataclasses import dataclass
-from torch.utils.tensorboard import SummaryWritter
+from torch.utils.tensorboard import SummaryWriter
 
 
-class representation_net(nn.Module):
+class representation_net(nn.Module): # h : state -> s^0
     def __init__(self):
         super().__init__()
         pass
 
-    def forward(self):
-        pass
+    def forward(self,x):
+        return state
 
-class dyanmic_net(nn.Module):
+class dynamic_net(nn.Module): # g : [s^k-1,a^k] -> [r^k,s^k]
     def __init__(self):
         super().__init__()
         pass
     
-    def forward(self,x):
-        pass
+    def forward(self,sk,ak):  
+        return rk,sk
 
-class prediction_net(nn.Module):
+class prediction_net(nn.Module): # f : s^k -> [p^k,v^k]
     def __init__(self):
         super().__innit__(self)
         pass
 
-    def forward(self,x):
-        pass
+    def forward(self,sk):
+        return pk,vk
 
 
 class mcts:
     def __innit__(self):
+        pass
+
+    def ucb(self):
+        pass
+
+    def selection(self):
+        pass
+
+    def expansion(self):
+        pass
+
+    def simulation(self):
+        pass
+
+    def backup(self):
         pass
 
 
@@ -63,6 +78,3 @@ class main:
     def train(self):
         pass
      
-
-
-
